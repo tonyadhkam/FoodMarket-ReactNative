00a8600089f4d2762db2bfc7d2d3d299951a3385
tonyadhkam <tonyandhika20@gmail.com>
2022-02-21 13:34:38 +0700

    Home Page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,26 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import React from 'react';
+import { ProfileDummy } from '../../assets';
 
 const Home = () => {
   return (
     <View>
-      <Text>Home Page Screen</Text>
+      <View style={styles.profileContainer}>
+        <View>
+          <Text style={styles.appName}>FoodMarket</Text>
+          <Text style={styles.desc}>Let's get some foods</Text>
+        </View>
+        <Image source={ProfileDummy} style={styles.profile}/>
+      </View>
     </View>
   );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  profile: {width: 50, height: 50, borderRadius: 8},
+  appName: {fontSize: 22, fontFamily: 'Poppins-Medium', color: '#020202'},
+  desc: {fontSize: 14, fontFamily: 'Poppins-Light', color: '#8D92A3'}, 
+  profileContainer: {flexDirection: 'row', justifyContent: 'space-between', paddingHorizontal: 24, paddingTop: 32, paddingBottom: 24, backgroundColor: 'white'}
+});
